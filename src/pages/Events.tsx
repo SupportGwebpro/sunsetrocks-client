55c010b9f458d0548bc1322322ef3e769b5e8965
Suvayan Mondal <suvayan@sourcedeskglobal.com>
2025-08-21 20:05:37 +0530

    feat: add event creation dialog with date picker and custom scrollbar styles
    - Added @radix-ui/react-dialog for modal functionality in Events page.
    - Integrated react-datepicker for selecting event dates.
    - Updated package.json to include new dependencies: @radix-ui/react-dialog, react-datepicker, and react-select.
    - Enhanced index.css with custom scrollbar styles.
    - Implemented a dialog for creating new events with input fields for event name, description, and date selection.

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,7 +1,11 @@
 import * as Tabs from "@radix-ui/react-tabs";
 import { ChevronDown, Edit, IconEye } from "../components/SVG";
 import { useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import DatePicker from "react-datepicker";
+
 export default function Events() {
+  const [selectedDate, setSelectedDate] = useState();
   return (
     <div className="px-12 py-10">
       <Tabs.Root defaultValue="0">
@@ -24,10 +28,124 @@ export default function Events() {
             </Tabs.List>
           </div>
           <div>
-            <button className="border border-[#F9298C] py-4 px-8 rounded-full flex items-center gap-4 bg-white cursor-pointer">
-              <span className="text-[#F9298C] text-[28px] leading-7">+</span>{" "}
-              <span>Create New Event</span>
-            </button>
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <button className="border border-[#F9298C] py-4 px-8 rounded-full flex items-center gap-4 bg-white cursor-pointer">
+                  <span className="text-[#F9298C] text-[28px] leading-7">
+                    +
+                  </span>
+                  <span>Create New Event</span>
+                </button>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                {/* Overlay */}
+                <Dialog.Overlay className="fixed inset-0 bg-[#00001D99] animate-fadeIn" />
+
+                {/* Content */}
+                <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[1406px] -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white shadow-lg focus:outline-none animate-scaleIn">
+                  {/* Title */}
+                  <div className="py-11 pl-20 pr-32 h-100 overflow-y-auto scrollbar-thin">
+                    <Dialog.Title className="text-black font-bold text-4xl leading-12 mb-5">
+                      Create Event
+                    </Dialog.Title>
+                    {/* Description */}
+                    <Dialog.Description className="mb-12 leading-6 text-black">
+                      Lorem ipsum dolor sit amet consectetur. Purus euismod
+                      turpis feugiat nisl tortor amet sit. Iaculis leo nisi
+                      volutpat mattis aliquam at in. Eu pulvinar nec enim
+                      fermentum vestibulum dolor turpis convallis.
+                    </Dialog.Description>
+                    <div className="flex gap-4 items-center mb-5">
+                      <label
+                        htmlFor="create-event-name"
+                        className="text-xl w-[170px] flex-shrink-0"
+                      >
+                        Event Name
+                        <span className="text-[#F9298C] ">*</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="create-event-name"
+                        id="create-event-name"
+                        placeholder="Event Name"
+                        className="border py-4.5 px-8 border-[#EAF0FF] focus:border-[#F9298C] transition-colors flex-grow bg-[#FBFBFB] outline-none rounded-xl"
+                      />
+                    </div>
+
+                    <div className="flex gap-4 items-center mb-5">
+                      <label
+                        htmlFor="create-event-description"
+                        className="text-xl w-[170px] flex-shrink-0"
+                      >
+                        Description
+                        <span className="text-[#F9298C] ">*</span>
+                      </label>
+                      <textarea
+                        name="create-event-description"
+                        id="create-event-description"
+                        placeholder="Add Description"
+                        className="border py-4.5 px-8 border-[#EAF0FF] focus:border-[#F9298C] transition-colors flex-grow bg-[#FBFBFB] outline-none rounded-xl min-h-[107px]"
+                      />
+                    </div>
+                    <div className="flex gap-4 items-center mb-5">
+                      <label className="text-xl w-[170px] flex-shrink-0">
+                        Date
+                        <span className="text-[#F9298C] ">*</span>
+                      </label>
+                      <div className="flex-grow">
+                        <div className="grid gap-4 grid-cols-3">
+                          <div>
+                            <DatePicker
+                              selected={selectedDate}
+                              onChange={(date) => setSelectedDate(date)}
+                              dateFormat="MMMM d, yyyy"
+                              className="border py-4.5 px-8 border-[#EAF0FF] focus:border-[#F9298C] transition-colors bg-[#FBFBFB] outline-none rounded-xl cursor-pointer text-left relative w-full"
+                              placeholderText="Event Start Date"
+                            />
+                          </div>
+                          <div>
+                            <DatePicker
+                              selected={selectedDate}
+                              onChange={(date) => setSelectedDate(date)}
+                              dateFormat="MMMM d, yyyy"
+                              className="border py-4.5 px-8 border-[#EAF0FF] focus:border-[#F9298C] transition-colors bg-[#FBFBFB] outline-none rounded-xl cursor-pointer text-left relative w-full"
+                              placeholderText="Event Start Date"
+                            />
+                          </div>
+                          <div>
+                            <DatePicker
+                              selected={selectedDate}
+                              onChange={(date) => setSelectedDate(date)}
+                              dateFormat="MMMM d, yyyy"
+                              className="border py-4.5 px-8 border-[#EAF0FF] focus:border-[#F9298C] transition-colors bg-[#FBFBFB] outline-none rounded-xl cursor-pointer text-left relative w-full"
+                              placeholderText="Event Start Date"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between bg-[#FFFAE3] py-12 px-24 rounded-b-3xl">
+                    <p>
+                      All fields marked with an asterisk (*) are required and
+                      cannot be left blank.
+                    </p>
+                    <div className="flex gap-5">
+                      <Dialog.Close asChild>
+                        <button className="text-[#F9298C] font-semibold px-6 py-2.5 border border-black/50 rounded-xl hover:text-white hover:border-[#F9298C] hover:bg-[#F9298C] transition-colors">
+                          Cancel
+                        </button>
+                      </Dialog.Close>
+                      <Dialog.Close asChild>
+                        <button className="text-[#F9298C]/50 bg-[#FFE6EB] font-semibold px-6 py-2.5 border border-[#FFE6EB] rounded-xl hover:text-white hover:border-[#F9298C] hover:bg-[#F9298C] transition-colors">
+                          Create Event
+                        </button>
+                      </Dialog.Close>
+                    </div>
+                  </div>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </div>
         </div>
         <div>
